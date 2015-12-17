90b9728da084ca5b3044ff15a61d56e5699f2700
Sam Lucas <smlucas13@gmail.com>
2015-12-17 16:36:06 -0500

    save

--- py/hellofunc.py
+++ py/hellofunc.py
@@ -3,11 +3,14 @@ import skilstak.colors as c
 def print_plain(name="Sam"):
     print("Hello " + name)
 
-def print_sir(name="Sir"):
-    print("Hello " + name)
+def print_hello(name="-h"):
+    print("Hello")
 
 def print_color(name="-c"):
     print("Hello " + (c.cyan + name))
 
 def print_multi(name="-m"):
-    print("Hello " + (c.multi + name))
+    print(c.multi("Hello " + name))
+
+def print_random(name="-r"):
+    print(c.random() + "Hello " + name)
